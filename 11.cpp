b9f5c4ea1e919b93d25ef39ca933e2c1483c9b3b
Srinivas2327 <59174061+Srinivas2327@users.noreply.github.com>
2019-12-31 11:57:09 +0500

    Add files via upload

---
+++ 11.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{ 
+int a;
+printf("enter number");
+scanf("%2d ",&a);
+printf("%d",a/10-a%10);
+return 0;
+
+}
---
+++ 2.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    char name[30];
+    puts("enter your name");
+    gets(name);
+    
+    puts("your name is");
+    puts(name);
+  return 0;  
+}
+
